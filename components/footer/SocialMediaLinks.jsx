a2da7d2c46dd04f52900418da1faaab77a274bdc
BattleRatttt <39086978+BattleRattt@users.noreply.github.com>
2022-09-11 03:51:34 -0500

    Update SocialMediaLinks.jsx

--- components/footer/SocialMediaLinks.jsx
+++ components/footer/SocialMediaLinks.jsx
@@ -6,27 +6,15 @@ export const SocialMediaLinks = (props) => (
   <ButtonGroup variant="ghost" color="gray.600" {...props}>
     <IconButton
       as="a"
-      href="https://discordapp.com/users/156165203619348480"
+      href="discord.gg/awVqQxVpJC"
       aria-label="Discord"
       icon={<FaDiscord fontSize="20px" />}
     />
     <IconButton
       as="a"
-      href="https://twitter.com/NicolaGenesin"
+      href="https://twitter.com/RadiantGaming"
       aria-label="Twitter"
       icon={<FaTwitter fontSize="20px" />}
-    />
-    <IconButton
-      as="a"
-      href="https://github.com/NicolaGenesin"
-      aria-label="GitHub"
-      icon={<FaGithub fontSize="20px" />}
-    />
-    <IconButton
-      as="a"
-      href="https://www.reddit.com/message/compose/?to=1911z"
-      aria-label="Reddit"
-      icon={<FaReddit fontSize="20px" />}
     />
   </ButtonGroup>
 );
